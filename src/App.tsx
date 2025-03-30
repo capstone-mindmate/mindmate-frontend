b9967b311b2ebb5c97cd00208521d40d9cdfdb42
sebeeeen <a856412@gmail.com>
2025-03-30 16:28:52 +0900

    🌟 feat : TopBar 컴포넌트 추가 - 제목, 뒤로가기 버튼, 액션 버튼의 기능 - 활성화/비활성화 액션 버튼 스타일
    Close MM-97

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import './App.css'
 import * as IconComponents from './components/icon/iconComponents'
 import { GlobalStyles } from '../styles/GlobalStyles'
+import { useToast } from './components/toast/ToastProvider.tsx'
+import TopBar from './components/topbar/Topbar.tsx'
 
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react'
@@ -17,9 +19,27 @@ const iconListStyles = css`
 `
 
 function App() {
+  const [count, setCount] = useState(0)
+  const { showToast } = useToast()
+
+  const handleBackClick = () => {
+    showToast('뒤로가기 버튼이 클릭되었습니다', 'info')
+  }
+
+  const handleAction = () => {
+    showToast('액션 버튼이 클릭되었습니다', 'success')
+  }
+
   return (
     <>
       <GlobalStyles />
+      <TopBar
+        title="타이틀 입력"
+        showBackButton
+        onBackClick={handleBackClick}
+        actionText="등록"
+        onActionClick={handleAction}
+      />
 
       <div
         style={{
@@ -28,15 +48,34 @@ function App() {
           alignItems: 'center',
           justifyContent: 'center',
           height: '100%',
+          paddingTop: '70px', // TopBar 높이만큼 여백 추가
         }}
       >
+        <div className="card" style={{ marginBottom: '30px' }}>
+          <button
+            onClick={() => {
+              setCount((count) => count + 1)
+              if (count % 4 === 0) {
+                showToast('카운트가 증가했습니다!', 'success')
+              } else if (count % 4 === 1) {
+                showToast('아주대학교 이메일을 입력해주세요', 'info')
+              } else if (count % 4 === 2) {
+                showToast('네트워크 연결을 확인해주세요', 'warning')
+              } else {
+                showToast('전송에 실패했습니다', 'error')
+              }
+            }}
+          >
+            count is {count}
+          </button>
+        </div>
+
         <h1>Icon List</h1>
         <div className="icon-list" css={iconListStyles}>
           <IconComponents.AlarmIcon color="#F7374F" />
           <IconComponents.BackIcon color="#F7374F" />
           <IconComponents.CheckIconBig color="#F7374F" />
           <IconComponents.CheckIconSmall color="#F7374F" />
-
           <IconComponents.ErrorIcon color="#F7374F" />
           <IconComponents.ImageIcon color="#F7374F" />
           <IconComponents.InfoIcon color="#F7374F" />
