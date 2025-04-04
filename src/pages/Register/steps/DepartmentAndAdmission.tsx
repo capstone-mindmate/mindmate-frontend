436e7d6ece387ba1ec21545b3f214e11094b4c4f
skfnxhjjj <skfnxhjjj@naver.com>
2025-04-05 01:28:33 +0900

    🎉 update : brownRectButton 스타일 관련 코드 수정 및 로컬 상태 관리 로직 추가

--- src/pages/Register/steps/DepartmentAndAdmission.tsx
+++ src/pages/Register/steps/DepartmentAndAdmission.tsx
@@ -8,9 +8,25 @@ import {
 } from './styles/InitialStyles'
 import TitleSelectBox from '../../../components/inputs/titleSelectBox'
 import BrownRectButton from '../../../components/buttons/brownRectButton'
-const handleInputChange = (value: string) => {}
+import { useState, useEffect } from 'react'
 
 const DepartmentAndAdmission = () => {
+  const [isEnabled, setIsEnabled] = useState(false)
+  const [selectedDepartment, setSelectedDepartment] = useState('')
+  const [selectedYear, setSelectedYear] = useState('')
+
+  useEffect(() => {
+    setIsEnabled(selectedDepartment !== '' && selectedYear !== '')
+  }, [selectedDepartment, selectedYear])
+
+  const handleDepartmentChange = (value: string) => {
+    setSelectedDepartment(value)
+  }
+
+  const handleYearChange = (value: string) => {
+    setSelectedYear(value)
+  }
+
   return (
     <RegisterContainer>
       <RegisterTitleContainer>
@@ -24,20 +40,24 @@ const DepartmentAndAdmission = () => {
       <RegisterInputContainer>
         <TitleSelectBox
           placeholder="학과를 선택해주세요"
-          onChange={handleInputChange}
+          onChange={handleDepartmentChange}
           titleText="학과"
           options={['학과1', '학과2', '학과3']}
         />
         <TitleSelectBox
           placeholder="입학년도를 선택해주세요"
-          onChange={handleInputChange}
+          onChange={handleYearChange}
           titleText="입학년도"
           options={['2020', '2021', '2022']}
         />
       </RegisterInputContainer>
 
       <RegisterConfirmButtonContainer>
-        <BrownRectButton buttonText="다음" onClick={() => {}} />
+        <BrownRectButton
+          isEnabled={isEnabled}
+          buttonText="다음"
+          onActiveChange={() => {}}
+        />
       </RegisterConfirmButtonContainer>
     </RegisterContainer>
   )
