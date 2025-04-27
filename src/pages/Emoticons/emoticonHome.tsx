e771b02daa43eae94d91ab30f942fd19cd5c5498
skfnxhjjj <skfnxhjjj@naver.com>
2025-04-27 17:54:48 +0900

    🌟 feat : 이모티콘샵 유저 프로필 컴포넌트 추가

--- src/pages/Emoticons/emoticonHome.tsx
+++ src/pages/Emoticons/emoticonHome.tsx
@@ -3,13 +3,19 @@ import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 import { RootContainer, EmoticonsContainer } from './style'
-
+import EmoticonProfile from '../../components/mypage/emoticonProfile'
 const EmoticonHome = () => {
   const navigate = useNavigate()
 
   return (
     <RootContainer>
-      <EmoticonsContainer></EmoticonsContainer>
+      <EmoticonsContainer>
+        <EmoticonProfile
+          profileImage="/public/image.png"
+          name="프로필이름"
+          heldCoins={100}
+        />
+      </EmoticonsContainer>
     </RootContainer>
   )
 }
