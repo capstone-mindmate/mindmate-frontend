db906f25bdb9f3fe9ca455e5c37aaacc3634bd2c
skfnxhjjj <skfnxhjjj@naver.com>
2025-05-05 04:45:53 +0900

    😎 fix : 버그 수정

--- src/components/navigation/navigationComponent.tsx
+++ src/components/navigation/navigationComponent.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
 /** @jsxImportSource @emotion/react */
+import React from 'react'
 import { css } from '@emotion/react'
 import { Link, useLocation } from 'react-router-dom'
 import {
