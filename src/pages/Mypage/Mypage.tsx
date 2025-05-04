42ddcfcc04e4e64dcccb28f6fe33978dd161eaf1
skfnxhjjj <skfnxhjjj@naver.com>
2025-05-04 17:24:44 +0900

    🎉 update : 프로필 페이지 완성에 따른 마이페이지 라우팅 추가

--- src/pages/Mypage/Mypage.tsx
+++ src/pages/Mypage/Mypage.tsx
@@ -15,7 +15,10 @@ import {
   MatchingGraphContainer,
 } from './MypageStyles'
 
+import { useNavigate } from 'react-router-dom'
+
 const MyPage = () => {
+  const navigate = useNavigate()
   // TODO: API 또는 상태 관리 라이브러리에서 사용자 정보를 가져오는 로직 추가
   // const { userProfile, userStats, userReviews } = useUserData() 같은 형태로 구현
 
@@ -30,14 +33,13 @@ const MyPage = () => {
 
   // TODO: 프로필 편집 버튼 클릭 핸들러
   const handleProfileEdit = () => {
-    console.log('프로필 편집 버튼이 클릭되었습니다')
+    navigate('/profile/edit')
     // TODO: 프로필 편집 페이지로 이동하는 로직 추가
   }
 
   // TODO: 설정 버튼 클릭 핸들러
   const handleSettingClick = () => {
-    console.log('설정 버튼이 클릭되었습니다')
-    // TODO: 설정 페이지로 이동 또는 설정 모달 표시 로직 추가
+    navigate('/profile/setting')
   }
 
   // TODO: 리뷰 전체보기 클릭 핸들러
