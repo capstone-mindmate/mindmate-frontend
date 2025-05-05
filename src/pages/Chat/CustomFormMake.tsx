09bf27bcef57133dbed09d6c428399c8047b9c07
skfnxhjjj <skfnxhjjj@naver.com>
2025-05-05 17:22:37 +0900

    🌟 feat : 커스텀폼 기초 파일 제작

---
+++ src/pages/Chat/CustomFormMake.tsx
@@ -0,0 +1,14 @@
+/** @jsxImportSource @emotion/react */
+import { useState, useEffect, useRef } from 'react'
+
+import { RootContainer } from './styles/RootStyles'
+
+interface CustomFormMakeProps {
+  matchId: string
+}
+
+const CustomFormMake = ({ matchId }: CustomFormMakeProps) => {
+  return <RootContainer></RootContainer>
+}
+
+export default CustomFormMake
---
+++ src/pages/Chat/CustomFormView.tsx
@@ -0,0 +1,14 @@
+/** @jsxImportSource @emotion/react */
+import { useState, useEffect, useRef } from 'react'
+
+import { RootContainer } from './styles/RootStyles'
+
+interface CustomFormViewProps {
+  matchId: string
+}
+
+const CustomFormView = ({ matchId }: CustomFormViewProps) => {
+  return <RootContainer></RootContainer>
+}
+
+export default CustomFormView
