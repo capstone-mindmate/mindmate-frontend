f10cdd4fe4aa05d0648d21898bfd8d7d5b10273b
skfnxhjjj <skfnxhjjj@naver.com>
2025-05-20 13:37:01 +0900

    😎 fix : nginx 라우팅 버그 해결

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
 
 export default defineConfig({
   plugins: [
@@ -34,26 +33,18 @@ export default defineConfig({
   ],
   server: {
     host: true,
+    proxy: {
+      '/api': {
+        target: 'https://mindmate.shop',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./src/tests/setup.ts'],
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          process: true,
-        }),
-      ],
-      define: {
-        global: 'globalThis',
-      },
-    },
-  },
-  define: {
-    global: 'globalThis',
   },
 })
