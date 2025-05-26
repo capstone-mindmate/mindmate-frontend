7447db2466471edf33c83dcaf96cd174d3d58d7f
skfnxhjjj <skfnxhjjj@naver.com>
2025-05-26 14:18:15 +0900

    😎 fix : 리뷰 태그 아이콘 렌더링 로직 추가

--- src/components/review/TagReview.tsx
+++ src/components/review/TagReview.tsx
@@ -35,6 +35,53 @@ const ArrowIconComponent: React.FC<{ isExpanded: boolean }> = ({
   )
 }
 
+const tagReMapper = (tag: TagItem) => {
+  switch (tag.text) {
+    case '응답이 빨라요':
+      return {
+        icon: '⚡️',
+        text: tag.text,
+        count: tag.count,
+      }
+    case '신뢰할 수 있는 대화였어요':
+      return {
+        icon: '🤝🏻',
+        text: tag.text,
+        count: tag.count,
+      }
+    case '공감을 잘해줘요':
+      return {
+        icon: '❤️‍🩹',
+        text: tag.text,
+        count: tag.count,
+      }
+    case '편안한 분위기에서 이야기할 수 있었어요':
+      return {
+        icon: '☕️️',
+        text: tag.text,
+        count: tag.count,
+      }
+    case '솔직하고 현실적인 조언을 해줘요':
+      return {
+        icon: '🎯️',
+        text: tag.text,
+        count: tag.count,
+      }
+    case '새로운 관점을 제시해줘요':
+      return {
+        icon: '💡️',
+        text: tag.text,
+        count: tag.count,
+      }
+    default:
+      return {
+        icon: tag.icon,
+        text: tag.text,
+        count: tag.count,
+      }
+  }
+}
+
 const TagReview: React.FC<TagReviewProps> = ({
   tags,
   initialVisibleCount = 3,
@@ -46,21 +93,24 @@ const TagReview: React.FC<TagReviewProps> = ({
   }
 
   const visibleTags = isExpanded ? tags : tags.slice(0, initialVisibleCount)
+  console.log(visibleTags)
 
   return (
     <TagReviewContainer>
       <TagReviewTitle>받은 평가 및 리뷰</TagReviewTitle>
 
       <TagListContainer>
-        {visibleTags.map((tag, index) => (
-          <TagItem key={index}>
-            <TagLeftSection>
-              <TagText>{tag.icon}</TagText>
-              <TagText>{tag.text}</TagText>
-            </TagLeftSection>
-            <TagCount>{tag.count}</TagCount>
-          </TagItem>
-        ))}
+        {visibleTags
+          .map((tag) => tagReMapper(tag))
+          .map((tag, index) => (
+            <TagItem key={index}>
+              <TagLeftSection>
+                <TagText>{tag.icon}</TagText>
+                <TagText>{tag.text}</TagText>
+              </TagLeftSection>
+              <TagCount>{tag.count}</TagCount>
+            </TagItem>
+          ))}
       </TagListContainer>
 
       {tags.length > initialVisibleCount && (
